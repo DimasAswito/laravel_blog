03a2fb61f75d7ebe3aba018739f98a0435d206ce
DimasAswito <dimasaswito@gmail.com>
2026-03-17 11:51:28 +0700

    Replace blog view with posts and update routes
    Remove the old blog view and introduce a posts listing: delete resources/views/blog.blade.php, add resources/views/posts.blade.php which iterates over a provided $posts array and renders excerpts (uses Str::limit). Update routes/web.php to serve /posts and pass sample post data and the page title. Update navbar links in resources/views/components/navbar.blade.php from /blog to /posts so navigation points to the new route.

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,20 @@ Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog Page']);
+Route::get('/posts', function () {
+    $posts = [
+        [
+            'title' => 'Judul Artikel 1',
+            'author' => 'Dimas Aswito',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, at consequuntur mollitia omnis, vitae sapiente dolorum nam perspiciatis nihil vel facere? Distinctio deleniti possimus temporibus excepturi, quibusdam inventore vel rerum!'
+        ],
+        [
+            'title' => 'Judul Artikel 2',
+            'author' => 'Dimas Aswito',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, at consequuntur mollitia omnis, vitae sapiente dolorum nam perspiciatis nihil vel facere? Distinctio deleniti possimus temporibus excepturi, quibusdam inventore vel rerum!'
+        ]
+    ];
+    return view('posts', ['title' => 'Blog Page', 'posts' => $posts]);
 });
 
 Route::get('/about', function () {
