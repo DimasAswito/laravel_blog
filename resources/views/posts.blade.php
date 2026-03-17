5aadca1f0c78186d9e1560c700e3258fdf35059f
DimasAswito <dimasaswito@gmail.com>
2026-03-17 13:24:03 +0700

    Add single post view and detail route
    Add resources/views/post.blade.php and update posts listing to link to individual posts by slug. Update routes/web.php to include id/slug fields in the sample posts, import Illuminate\Support\Arr, and add a /posts/{slug} route that looks up a post by slug (using Arr::first), returns the single post view, and aborts with 404 if not found. Also wrap post preview markup with an anchor in posts.blade.php and update the read-more/back links.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,13 +2,15 @@
   @foreach ($posts as $post)
     
   <article class="py-8 max-w-3xl border-b border-gray-300">
-    <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
-    <div class="text-base text-gray-500">
-      <a href="#">{{ $post ['author'] }}</a> | 1 Januari 2026
-    </div>
-    <p class="my-4 font-light">{{ Str::limit($post['body'],130) }}</p>
-    <a href="#" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
-  </article>
+    <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
+      <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
+      <div class="text-base text-gray-500">
+        <a href="#">{{ $post ['author'] }}</a> | 1 Januari 2026
+      </div>
+      <p class="my-4 font-light">{{ Str::limit($post['body'],130) }}</p>
+      <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
+    </a>
+    </article>
   
   @endforeach
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,16 +11,47 @@ Route::get('/posts', function () {
     $posts = [
         [
             'title' => 'Judul Artikel 1',
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
             'author' => 'Dimas Aswito',
             'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, at consequuntur mollitia omnis, vitae sapiente dolorum nam perspiciatis nihil vel facere? Distinctio deleniti possimus temporibus excepturi, quibusdam inventore vel rerum!'
         ],
         [
             'title' => 'Judul Artikel 2',
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
             'author' => 'Dimas Aswito',
             'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, at consequuntur mollitia omnis, vitae sapiente dolorum nam perspiciatis nihil vel facere? Distinctio deleniti possimus temporibus excepturi, quibusdam inventore vel rerum!'
         ]
     ];
     return view('posts', ['title' => 'Blog Page', 'posts' => $posts]);
+});
+
+Route::get('/posts/{slug}', function($slug){
+    $posts = [
+        [
+            'title' => 'Judul Artikel 1',
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'author' => 'Dimas Aswito',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, at consequuntur mollitia omnis, vitae sapiente dolorum nam perspiciatis nihil vel facere? Distinctio deleniti possimus temporibus excepturi, quibusdam inventore vel rerum!'
+        ],
+        [
+            'title' => 'Judul Artikel 2',
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'author' => 'Dimas Aswito',
+            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores, at consequuntur mollitia omnis, vitae sapiente dolorum nam perspiciatis nihil vel facere? Distinctio deleniti possimus temporibus excepturi, quibusdam inventore vel rerum!'
+        ]
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug) {
+        return $post['slug'] == $slug;
+    });
+
+    if(!$post) abort(404);
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/about', function () {
